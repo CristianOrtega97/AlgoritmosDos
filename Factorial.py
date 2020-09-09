8238585389007e276e22c580c136b6ae8968e0ce
CristianOrtega97 <emanuel.ortega@hotmail.com>
2020-09-08 21:48:09 -0500

    Ejercicio Factorial

---
+++ Factorial.py
@@ -0,0 +1,21 @@
+def factorial (numero):
+        if numero == 1:
+            return numero
+        else:
+            try:
+                return numero * factorial(numero-1)
+            except RecursionError:
+                print('Ingrese un número mayor o igual a 1')
+
+numero = input('Ingrese un número: ')
+if numero.isnumeric():
+        numero=int(numero)
+        print('El factorial del numero es: ',factorial(numero))  
+else:
+    numero=ord(numero)
+    if numero>=65 and numero<=90:
+        print('El factorial del numero es: ',factorial(numero))
+    elif numero>=97 and numero<=122:
+        print('El factorial del numero es: ',factorial(numero))
+    else:
+        print('Ingrese una letra MAYÚSCULA O minuscula')
